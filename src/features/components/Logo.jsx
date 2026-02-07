ca94a4a2feb126f430b78795587480100f8d7bdd
nott-po <nesterovapolina55@gmail.com>
2026-02-07 15:27:42 +0100

    refactor: remove obsolete pages and layout components

--- src/features/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-    return (
-        <div className="nav-header">
-          <img src="/logo.svg" alt="Restaurant CRM Logo" className="nav-logo" />
-          <hr className="nav-divider" />
-        </div>
-    );
-}
